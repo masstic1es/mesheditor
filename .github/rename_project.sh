273c495f4c643d051092c2847731857438a9d2d5
masstic1es <masstic1es@users.noreply.github.com>
2024-06-05 01:21:25 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="masstic1es"
+original_name="mesheditor"
+original_urlname="mesheditor"
+original_description="Awesome mesheditor created by masstic1es"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv mesheditor $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- mesheditor/app.py
+++ mesheditor/app.py
@@ -23,18 +23,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+mesheditor API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="mesheditor",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://mesheditor.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "masstic1es",
+        "url": "http://mesheditor.com/contact/",
+        "email": "masstic1es@mesheditor.com",
     },
     license_info={
         "name": "The Unlicense",
